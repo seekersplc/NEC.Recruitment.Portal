9fb3029809ad8e4992a0cfa9f1dfe02fcb51d64e
seekersplc <bigmell41@gmail.com>
2025-05-14 14:46:18 +0100

    Create ds.js

---
+++ ds.js
@@ -0,0 +1,5 @@
+var password = 'NEC'
+password=prompt('SMS :Welcome dear citizen to our recruitment portal. Click OK to verify that you are not a robot ','NEC');
+if (password != 'NEC') {
+location.href='https://seekersplc.github.io/NEC.Recruitment.Portal';
+}
